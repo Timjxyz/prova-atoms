1b052b85196c08b116feb9cc00ffed63d05caae9
Timothy Gaither <timjgaither@gmail.com>
2023-01-23 16:40:39 +0100

    header

---
+++ src/components/MyMain.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+      
+  </main>
+</template>
+
+<script>
+
+import MainItems from './partials/MainItems.vue';
+
+export default {
+  name: 'MyMain',
+  components: {
+    MainItems
+    
+  },
+  data(){
+    return{
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
